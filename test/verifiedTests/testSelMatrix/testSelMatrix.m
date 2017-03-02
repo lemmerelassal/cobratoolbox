202464694a3ae77072bc99e785154a8c61e70931
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2017-03-02 10:10:40 +0100

    Fixed testSelMatrix.m to use correct structure

--- test/verifiedTests/testSelMatrix/testSelMatrix.m
+++ test/verifiedTests/testSelMatrix/testSelMatrix.m
@@ -1,19 +1,41 @@
-% Tests functionality of selMatrix.
+% The COBRAToolbox: testSelMatrix.m
+%
+% Purpose:
+%     - testSelMatrix tests the functionality of selMatrix()
+%       and checks solution against a known solution.
+%
+% Authors:
+%     - Lemmer El Assal February 2017
+%
 
-%save original directory
-oriDir = pwd;
+% define the path to The COBRAToolbox
+pth = which('initCobraToolbox.m');
+CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
 
-%change to test folder
-mFilePath = mfilename('fullpath');
-cd(mFilePath(1:end-length(mfilename)));
+initTest([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
 
-load ref.mat;
+ref_selMat1 = zeros(2,6);
+ref_selMat2 = zeros(2,6);
+
+ref_selMat1(1, 1) = 1;
+ref_selMat1(2, 4) = 1;
+
+ref_selMat2(1, 1) = 1;
+ref_selMat2(2, 4) = 1;
+ref_selMat2(2, 5) = -1;
+
+ref_selMat1 = sparse(ref_selMat1);
+ref_selMat2 = sparse(ref_selMat2);
+
+selVec1 = [1, 0, 0, 1, 0, 0];
+selVec2 = [1, 0, 0, 1, -1, 0];
+
 
 selMat1 = selMatrix(selVec1);
 selMat2 = selMatrix(selVec2);
-assert(isequal(selMat1,ref_selMat1))
-assert(isequal(selMat2,ref_selMat2))
+assert(isequal(selMat1, ref_selMat1))
+assert(isequal(selMat2, ref_selMat2))
 
 
-%return to original directory
-cd(oriDir);
+% change the directory
+cd(CBTDIR)
